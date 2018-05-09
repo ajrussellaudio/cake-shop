b5efac7e969f5ff5082dd2ac68e6ced026a30085
Alan Russell <ajrussellaudio@gmail.com>
2018-05-09 17:27:07 +0100

    add cake detail component

---
+++ src/components/CakeDetail.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CakeDetail from "./CakeDetail";
+
+describe("CakeDetail", () => {
+  it("should display the comment/review made");
+});
