ce7787f2c1107d474347ba71f314dfcdf2303e24
Alan Russell <ajrussellaudio@gmail.com>
2018-05-09 17:18:15 +0100

    add live data to cake view

--- src/components/CakeView.js
+++ src/components/CakeView.js
@@ -1,8 +1,24 @@
 import React from "react";
 
-const CakeView = ({ match }) => {
-  console.log(match.params.id);
-  return <h1>CakeView: {match.params.id}</h1>;
-};
+class CakeView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const baseUrl =
+      "http://ec2-34-243-153-154.eu-west-1.compute.amazonaws.com:5000/api";
+    const id = this.props.match.params.id;
+    fetch(`${baseUrl}/cakes/${id}`)
+      .then(res => res.json())
+      .then(cake => this.setState({ ...cake }));
+  }
+
+  render() {
+    if (!this.state.id) return null;
+    return <h3>{this.state.name}</h3>;
+  }
+}
 
 export default CakeView;
