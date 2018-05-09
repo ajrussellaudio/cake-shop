82cbafd306e9a19e51e3e0f852e33ce9110831b6
Alan Russell <ajrussellaudio@gmail.com>
2018-05-09 22:40:56 +0100

    adjust styles of cake detail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CakesIndex from "./views/CakesIndex";
 import CakeView from "./views/CakeView";
 
 const App = () => (
   <Router>
     <div className="App">
-      <h1>Cakes!</h1>
+      <header>
+        <Link to="/">
+          <h1>Cakes!</h1>
+        </Link>
+      </header>
       <Route exact path="/" component={CakesIndex} />
       <Route path="/:id" component={CakeView} />
     </div>
--- src/components/CakeDetail.js
+++ src/components/CakeDetail.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./CakeDetail.css";
 
 const CakeDetail = ({ name, imageUrl, comment, yumFactor }) => (
-  <div className="cake-detail" style={{ backgroundImage: `url(${imageUrl})` }}>
-    <h3>{name}</h3>
-    <p className="comment">{comment}</p>
-    <p className="yum-factor">Yum Factor: {yumFactor}</p>
+  <div className="container">
+    <div
+      className="cake-detail"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      <div>
+        <h3>{name}</h3>
+        <p className="comment">{comment}</p>
+        <p className="yum-factor">Yum Factor: {yumFactor}</p>
+        <Link to="/">Back to cakes</Link>
+      </div>
+    </div>
   </div>
 );
 
