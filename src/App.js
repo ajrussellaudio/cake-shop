e7c54d209447fa8bda0b0fc59ead0047f5c29426
Alan Russell <ajrussellaudio@gmail.com>
2018-05-09 14:30:23 +0100

    load live data into app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from "react";
 import CakeList from "./components/CakeList";
 
 class App extends React.Component {
+  state = {
+    cakes: []
+  };
+
+  componentDidMount() {
+    const baseUrl =
+      "http://ec2-34-243-153-154.eu-west-1.compute.amazonaws.com:5000/api";
+    fetch(`${baseUrl}/cakes`)
+      .then(res => res.json())
+      .then(cakes => this.setState({ cakes: cakes }));
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Cakes!</h1>
-        <CakeList cakes={[]} />
+        <CakeList cakes={this.state.cakes} />
       </div>
     );
   }
