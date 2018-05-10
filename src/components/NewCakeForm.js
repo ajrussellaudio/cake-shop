8935d3eb16355eac3686fe0e25a7c0753bd3be43
Alan Russell <ajrussellaudio@gmail.com>
2018-05-10 11:13:22 +0100

    styling tweaks

--- src/components/NewCakeForm.js
+++ src/components/NewCakeForm.js
@@ -1,49 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./NewCakeForm.css";
 
 const NewCakeForm = props => (
-  <form onSubmit={props.onSubmit}>
-    <label htmlFor="name">Name:</label>
-    <input
-      type="text"
-      id="name"
-      name="name"
-      value={props.name}
-      onChange={props.onPropertyChange}
-    />
+  <div id="form-container">
+    <form onSubmit={props.onSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={props.name}
+        onChange={props.onPropertyChange}
+      />
 
-    <label htmlFor="comment">Comment:</label>
-    <textarea
-      name="comment"
-      id="comment"
-      cols="30"
-      rows="10"
-      value={props.comment}
-      onChange={props.onPropertyChange}
-    />
+      <label htmlFor="comment">Comment:</label>
+      <textarea
+        name="comment"
+        id="comment"
+        cols="30"
+        rows="4"
+        value={props.comment}
+        onChange={props.onPropertyChange}
+      />
 
-    <label htmlFor="image-url">Image URL:</label>
-    <input
-      type="text"
-      id="image-url"
-      name="imageUrl"
-      value={props.imageUrl}
-      onChange={props.onPropertyChange}
-    />
+      <label htmlFor="image-url">Image URL:</label>
+      <input
+        type="text"
+        id="image-url"
+        name="imageUrl"
+        value={props.imageUrl}
+        onChange={props.onPropertyChange}
+      />
 
-    <label htmlFor="yum-factor">Yum Factor:</label>
-    <input
-      type="number"
-      id="yum-factor"
-      name="yumFactor"
-      min={1}
-      max={5}
-      value={parseInt(props.yumFactor, 10)}
-      onChange={props.onPropertyChange}
-    />
+      <label htmlFor="yum-factor">Yum Factor:</label>
+      <input
+        type="number"
+        id="yum-factor"
+        name="yumFactor"
+        min={1}
+        max={5}
+        value={parseInt(props.yumFactor, 10)}
+        onChange={props.onPropertyChange}
+      />
 
-    <input type="submit" id="submit" value="New Cake" />
-  </form>
+      <input type="submit" id="submit" value="Make a New Cake!" />
+    </form>
+  </div>
 );
 
 NewCakeForm.propTypes = {
