eeea09486156fda10c9927fc789207037d0b1fc3
Alan Russell <ajrussellaudio@gmail.com>
2018-05-10 12:26:47 +0100

    make app component stateless

--- src/App.js
+++ src/App.js
@@ -4,38 +4,22 @@ import CakesIndex from "./views/CakesIndex";
 import CakeView from "./views/CakeView";
 import CakeCreate from "./views/CakeCreate";
 
-class App extends React.Component {
-  state = {
-    baseUrl: ""
-  };
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header>
-            <Link to="/">
-              <h1>Cakes!</h1>
-            </Link>
-            <Link to="/new" className="new-cake">
-              New Cake
-            </Link>
-          </header>
-          <Route exact path="/" render={() => <CakesIndex {...this.state} />} />
-          <Route
-            exact
-            path="/new"
-            render={() => <CakeCreate {...this.state} />}
-          />
-          <Route
-            exact
-            path="/cakes/:id"
-            render={match => <CakeView {...this.state} {...match} />}
-          />
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header>
+        <Link to="/">
+          <h1>Cakes!</h1>
+        </Link>
+        <Link to="/new" className="new-cake">
+          New Cake
+        </Link>
+      </header>
+      <Route exact path="/" component={CakesIndex} />
+      <Route exact path="/new" component={CakeCreate} />
+      <Route exact path="/cakes/:id" component={CakeView} />
+    </div>
+  </Router>
+);
 
 export default App;
